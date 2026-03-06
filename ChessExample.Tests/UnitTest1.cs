b92d4a849a4a07fb460bc7b2d8d243a9b6aeb9fc
SAJI <nmesrobyan@inbox.ru>
2026-03-06 17:55:46 +0500

    тестирование метода CheckerBoardPositionTests

--- ChessExample.Tests/UnitTest1.cs
+++ ChessExample.Tests/UnitTest1.cs
@@ -1,5 +1,110 @@
 namespace ChessExample.Tests
 {
+    public class CheckerBoardPositionTests
+    {
+        [Fact]
+        public void ConstructorValidCoordinatesSuccess()
+        {
+            var pos = new CheckerBoardPosition(1, 1);
+
+            Assert.Equal(1, pos.X);
+            Assert.Equal(1, pos.Y);
+        }
+
+        [Fact]
+        public void ConstructorInvalidXThrowsException()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+                new CheckerBoardPosition(0, 1));
+        }
+
+        [Fact]
+        public void ConstructorInvalidYThrowsException()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+                new CheckerBoardPosition(1, 9));
+        }
+
+        [Fact]
+        public void XLetterReturnsCorrectLetter()
+        {
+            var pos = new CheckerBoardPosition(1, 5);
+
+            Assert.Equal('A', pos.XLetter);
+        }
+
+        [Fact]
+        public void XLetterHColumnReturnsH()
+        {
+            var pos = new CheckerBoardPosition(8, 1);
+
+            Assert.Equal('H', pos.XLetter);
+        }
+
+        [Fact]
+        public void ToStringReturnsChessNotation()
+        {
+            var pos = new CheckerBoardPosition(5, 2);
+
+            var result = pos.ToString();
+
+            Assert.Equal("E2", result);
+        }
+
+        [Theory]
+        [InlineData("A1", 1, 1)]
+        [InlineData("E2", 5, 2)]
+        [InlineData("H8", 8, 8)]
+        public void TryParseValidInputReturnsTrue(string input, byte x, byte y)
+        {
+            var success = CheckerBoardPosition.TryParse(input, null, out var result);
+
+            Assert.True(success);
+            Assert.NotNull(result);
+            Assert.Equal(x, result.X);
+            Assert.Equal(y, result.Y);
+        }
+
+        [Theory]
+        [InlineData("Z9")]
+        [InlineData("A9")]
+        [InlineData("I1")]
+        [InlineData("AA")]
+        [InlineData("")]
+        public void TryParseInvalidInputReturnsFalse(string input)
+        {
+            var success = CheckerBoardPosition.TryParse(input, null, out var result);
+
+            Assert.False(success);
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public void TryParseNullReturnsFalse()
+        {
+            var success = CheckerBoardPosition.TryParse(null, null, out var result);
+
+            Assert.False(success);
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public void ParsInvalidInputThrowsException()
+        {
+            Assert.Throws<FormatException>(() =>
+                CheckerBoardPosition.Parse("Z9", null));
+        }
+
+        [Fact]
+        public void ParseValidInputReturnsPosition()
+        {
+            var pos = CheckerBoardPosition.Parse("E2", null);
+
+            Assert.Equal(5, pos.X);
+            Assert.Equal(2, pos.Y);
+        }
+    }
+
     public class ChessFiguresTests
     {
         [Fact]
